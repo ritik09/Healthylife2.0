28e5df8b92538274f136b8b50b7cb7be0cc4509b
ritik09 <ritikgupta0912@gmail.com>
2021-10-05 23:30:12 +0530

    update settings.py

--- server/tutorial/tutorial/settings.py
+++ server/tutorial/tutorial/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/2.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/2.2/ref/settings/
 """
-
+import django_heroku
 import os
 import datetime
 
@@ -258,3 +258,4 @@ CHANNEL_LAYERS = {
         },
     },
 }
+django_heroku.settings(locals())
